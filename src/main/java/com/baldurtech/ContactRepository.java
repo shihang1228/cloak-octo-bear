3150e5a9b8e90c94b8b7acb05a12c7bafdf5fe41
shihang <study1228.sh@gmail.com>
2014-08-16 10:23:54 +0800

    实现了ContactRepository的deleteById

--- src/main/java/com/baldurtech/ContactRepository.java
+++ src/main/java/com/baldurtech/ContactRepository.java
@@ -2,12 +2,13 @@ package com.baldurtech;
 
 public class ContactRepository
 {
+    private DatabaseManager db;
     public ContactRepository(DatabaseManager db)
     {
-
+        this.db = db;
     }
     public void deleteById(Long id)
     {
-        
+        db.executeUpdate("DELETE FROM contact WHERE id=" + id);
     }
 }
