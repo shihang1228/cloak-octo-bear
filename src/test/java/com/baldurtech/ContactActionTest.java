7dc5456607245b6eddb9020c5d03bdc176ae6545
shihang <study1228.sh@gmail.com>
2014-08-26 18:58:51 +0800

    重构测试ContactActionTest

--- src/test/java/com/baldurtech/ContactActionTest.java
+++ src/test/java/com/baldurtech/ContactActionTest.java
@@ -3,12 +3,21 @@ package com.baldurtech;
 import com.baldurtech.unit.TestCase;
 public class ContactActionTest extends TestCase
 {   
+    ContactManagerMock contactManager;
+    ActionContextMock actionContext;
+    ContactAction action;
+    //工龄工资涨多少，，，
+    public void setUp()
+    {
+        contactManager = new ContactManagerMock();
+        actionContext = new ActionContextMock();
+        action = new ContactAction(actionContext, contactManager);
+    }
+
     public void test_show_当id为空的时候应该跳转到list()
     {
-        ContactManagerMock contactManager = new ContactManagerMock();
-        ActionContextMock actionContext = new ActionContextMock();
+        setUp();
         actionContext.setParameter("id", null);
-        ContactAction action = new ContactAction(actionContext, contactManager);
         action.show();
         
         assertEquals("contact/list", actionContext.redirectActionParam);
@@ -16,19 +25,20 @@ public class ContactActionTest extends TestCase
     
     public void test_save_保存成功后要跳到list()
     {
-        ActionContextMock actionContext = new ActionContextMock();
+        setUp();
         actionContext.setParameter("id", null);
-        
-        Contact contact = new Contact();
-        contact.setId(1L);
-        contact.setPersistent(true);
-        
-        ContactManagerMock contactManager = new ContactManagerMock();
-        contactManager.saveShouldReturn = contact;
-        
-        ContactAction contactAction = new ContactAction(actionContext, contactManager);
-        contactAction.save();
+        contactManager.saveShouldReturn = createPersistentContactWithId(1L);     
+        action.save();
         
         assertEquals("contact/list", actionContext.redirectActionParam);
+    }
+    
+    public Contact createPersistentContactWithId(Long id)
+    {
+        Contact contact = new Contact();
+        contact.setId(id);
+        contact.setPersistent(true);
+        
+        return contact;
     }
 }
